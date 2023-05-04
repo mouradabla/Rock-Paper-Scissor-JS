b0fc6449f6162462128729afd3fc949bd0394fda
Torstein S. Ølberg <mr.torstein@gmail.com>
2023-05-04 17:31:49 +0200

    Added cancel button and user input error handeling

--- js/game.js
+++ js/game.js
@@ -32,13 +32,40 @@ const computer_selection = computerPlay();
  * @returns {string} User input choices of "Rock", "Paper", or "Scissors".
  */
 function player_play() {
+    let userInput = false;
+    let playerSelection;
 
-    // Asks for input from user
-    let userInput = prompt("Please input either rock, paper or scissors: ");
-
-    // Forces lower case, then title case
-    userInput = userInput.toLowerCase();
-    userInput = userInput.replace(userInput[0], userInput[0].toUpperCase());
+    // Asks for input from user until valid input is given
+    while (userInput == false) {
+        try {
+            userInput = prompt("Please input either rock, paper or scissors: ");
+            playerSelection = userInput.toLowerCase();
+        }
+        catch (TypeError) {
+            // Handle cancel button error
+            playerSelection = "exit";
+        }
+        
+        // Forces title case
+        playerSelection = playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase());
+        
+        // Handle invalid user inputs
+        switch (playerSelection) {
+            case "Rock":
+                break;
+            case "Paper":
+                break;
+            case "Scissors":
+                break;
+            case "Exit":
+                break;
+            case false:
+                break;
+            default:
+                console.log(`'${userInput}' is an invalid input. Please input either Rock, Paper or Scissors`);
+                userInput = false;
+        }
+    }
 
     // return the user input
     return userInput;
