342177d496379d764b625179f02f1ea8c684ca7e
MouradAbla <64438466+mouradabla@users.noreply.github.com>
2023-05-05 13:28:28 +0200

    done most of the game - but need CodeReview please

--- js/game.js
+++ js/game.js
@@ -1,3 +1,7 @@
+// constant array contains all the possible choices, 3, from the computer
+const CHOICES = [`Rock`, `Paper`, `Scissors`];
+let playerWins = 0;
+const WINING_SCORE = 5;
 
 /**
  * Returns a randomly generated choices of 'Rock', 'Paper', or 'Scissors'.
@@ -8,18 +12,77 @@
  * 
  * @returns {string} A randomly generated choices of 'Rock', 'Paper', or 'Scissors'.
  */
-function computerPlay() {
-
-    // constant array contains all the possible choices, 3, from the computer
-    const choices = [`Rock`, `Paper`, `Scissors`];
+function computer_play() {
 
     // random number between 0 and 2
-    const random_index = Math.floor(Math.random() * choices.length);
+    const randomIndex = Math.floor(Math.random() * CHOICES.length);
 
     // return the random choice from the array choices
-    return choices[random_index];
+    return CHOICES[randomIndex];
 }
 
-// computer_selection holds the random choices result
-const computer_selection = computerPlay();
+function player_play(message = 'Please enter "Rock", "Paper" or "Scissors": ') {
 
+    let userInput = prompt(message);
+
+    // Check if player's Cancel or enter empty value break;
+    if (userInput === null || userInput === "") {
+        return "Exit";
+    }
+
+    userInput = userInput.toLocaleLowerCase();
+    userInput = userInput.replace(userInput[0], userInput[0].toUpperCase());
+
+    // Check if player's input is invalid
+    while (!CHOICES.includes(userInput)) {
+        userInput = prompt(`Invalid choice! Please enter "Rock", "Paper", or "Scissors"`);
+
+        userInput = userInput.toLocaleLowerCase();
+        userInput = userInput.replace(userInput[0], userInput[0].toUpperCase());
+    }
+
+    return userInput;
+}
+
+function play_round(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+
+        console.log(`It's a Tie! ${playerSelection} V.S ${computerSelection}`);
+
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+        playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper") {
+
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        playerWins++;
+        console.log(`Your score: ${playerWins} wins!`);
+
+    } else {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    }
+}
+
+function game_play() {
+
+    //
+    while (playerWins < WINING_SCORE) {
+
+        // computer_selection holds the random choices result
+        const computerSelection = computer_play();
+        const playerSelection = player_play();
+
+        if (playerSelection !== "Exit") {
+
+            play_round(playerSelection, computerSelection);
+        } else {
+            console.log(`Game will stop now! Your score is: ${playerWins}`)
+
+            console.log("Please refresh the page to play again!")
+
+            break;
+        }
+    }
+}
+
+game_play();
